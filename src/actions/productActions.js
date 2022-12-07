17f91a33999760749992585463634ba066617c3e
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-12-07 11:37:39 +0300

    GET products-actions

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,1 +1,21 @@
 const BASE_URL = 'http://localhost:3001/api/v1';
+
+//filter fetch-/products
+export const fetchProducts = () => dispatch => {
+    fetch(`${BASE_URL}/products`)
+      .then(res => res.json())
+      .then(products => dispatch({ type: 'FETCH_PRODUCTS', payload: products }));
+  };
+  
+  //filter search
+  export const filterProducts = (products, filter) => dispatch => {
+    const filteredProducts = products.filter(product => product.name.toUpperCase().indexOf(filter.toUpperCase()) > -1);
+  
+    dispatch({
+      type: 'FILTER_PRODUCTS_BY_SEARCH',
+      payload: {
+        filter: filter,
+        filteredProducts: filteredProducts,
+      },
+    });
+  };
