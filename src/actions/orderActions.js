6c5de8283ff2999027354c65d97c1c15aa63fed6
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-12-07 12:20:24 +0300

    POST /orders

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -1,1 +1,15 @@
 const BASE_URL = 'http://localhost:3001/api/v1';
+
+export const createOrder = order => dispatch => {
+    fetch(`${BASE_URL}/orders`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ order }),
+    })
+      .then(res => res.json())
+      .then(order => dispatch({ type: 'CREATE_ORDER', payload: order, success: true }));
+  };
+
+  
