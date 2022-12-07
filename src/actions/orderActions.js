09b02129c4a5c933932ec39595cdfe4d08536a7c
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-12-07 12:23:37 +0300

    GET orders/display orders

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -12,4 +12,24 @@ export const createOrder = order => dispatch => {
       .then(order => dispatch({ type: 'CREATE_ORDER', payload: order, success: true }));
   };
 
+  export const fetchOrder = orderId => dispatch => {
+    fetch(`${BASE_URL}/orders/${orderId}`)
+      .then(res => res.json())
+      .then(order => dispatch({ type: 'FETCH_ORDER', payload: order, success: true }));
+  };
+
+  export const listMyOrders = () => dispatch => {
+    fetch(`${BASE_URL}/orders`)
+      .then(res => res.json())
+      .then(orders => dispatch({ type: 'FETCH_MY_ORDERS', payload: orders, success: true }));
+  };
   
+  export const fetchOrders = () => dispatch => {
+    fetch(`${BASE_URL}/orders`)
+      .then(res => res.json())
+      .then(orders => dispatch({ type: 'FETCH_ORDERS', payload: orders, success: true }));
+  };
+
+  
+
+
