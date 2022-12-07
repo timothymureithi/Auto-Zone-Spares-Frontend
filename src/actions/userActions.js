6afb35060d16e23ce2af1ca9f7f298b2e242406b
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-12-07 10:52:23 +0300

    POST /users

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -12,4 +12,22 @@ export const signin = (email, password) => dispatch => {
       .then(user => dispatch({ type: 'LOGIN_USER', payload: user }));
   };
 
+  export const register = (first_name, last_name, email, password, password_confirmation) => dispatch => {
+    fetch(`${BASE_URL}/users`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        first_name,
+        last_name,
+        email,
+        password: password,
+        password_confirmation: password_confirmation,
+      }),
+    })
+      .then(res => res.json())
+      .then(user => dispatch({ type: 'REGISTER_USER', payload: user }));
+  };
+
   
