5bebb040ca8fb7e47a5e328da56a55abcdeb2c0c
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-12-08 19:35:02 +0300

    set up Profile Container

---
+++ src/containers/userContainers/ProfileContainer.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import { logout, update } from '../../actions/userActions';
+import { useSelector, useDispatch } from 'react-redux';
+import { listMyOrders } from '../../actions/orderActions';
+import { Link } from 'react-router-dom';
+import formatCurrency from '../../util';
+
+
+const ProfileContainer = props => {
+
+};
+
+export default ProfileContainer;
