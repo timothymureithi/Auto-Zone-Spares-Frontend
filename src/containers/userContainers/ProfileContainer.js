2df6df8af111fe576b8d891489a92113e84a6d01
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-12-08 22:07:11 +0300

    Updated App.js and  css files

--- src/containers/userContainers/ProfileContainer.js
+++ src/containers/userContainers/ProfileContainer.js
@@ -3,25 +3,25 @@ import { logout, update } from '../../actions/userActions';
 import { useSelector, useDispatch } from 'react-redux';
 import { listMyOrders } from '../../actions/orderActions';
 import { Link } from 'react-router-dom';
-import formatCurrency from '../../util';
-
 
 const ProfileContainer = props => {
-    
-    const [password, setPassword] = useState('');
-    const [passwordConfirmation, setPasswordConformation] = useState('');
-    const { userInfo } = useSelector(state => state.user);
-    const { success, myOrders } = useSelector(state => state.orders);
-    const myNewOrders = myOrders
+  const dispatch = useDispatch();
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConformation] = useState('');
+  const { userInfo } = useSelector(state => state.user);
+  const { success, myOrders } = useSelector(state => state.orders);
+  const myNewOrders = myOrders
     .filter(order => order.user.id === userInfo.user.id)
     .sort((a, b) => (a.created_at < b.created_at ? 1 : -1));
-    
-    const handleLogout = () => {
-        dispatch(logout());
-        props.history.push('/signin');
-      };
-    
-  
+
+  const handleLogout = () => {
+    dispatch(logout());
+    props.history.push('/signin');
+  };
+
   const submitHandler = e => {
     e.preventDefault();
     dispatch(update(userInfo.user.id, firstName, lastName, email, password, passwordConfirmation));
@@ -41,13 +41,13 @@ const ProfileContainer = props => {
 
   return (
     <div className='profile'>
-    <div className='profile-info'>
-      <form onSubmit={submitHandler}>
-        <ul className='form-container'>
-          <li>
-            <h2>User Profile</h2>
-          </li>
-          <li>
+      <div className='profile-info'>
+        <form onSubmit={submitHandler}>
+          <ul className='form-container'>
+            <li>
+              <h2>User Profile</h2>
+            </li>
+            <li>
               <label htmlFor='first_name'>First Name</label>
               <input
                 value={firstName}
@@ -57,7 +57,7 @@ const ProfileContainer = props => {
                 id='first_name'
                 onChange={e => setFirstName(e.target.value)}
               />
-              </li>
+            </li>
             <li>
               <label htmlFor='last_name'>Last Name</label>
               <input
@@ -68,6 +68,7 @@ const ProfileContainer = props => {
                 id='last_name'
                 onChange={e => setLastName(e.target.value)}
               />
+            </li>
             <li>
               <label htmlFor='email'>Email</label>
               <input
@@ -78,7 +79,7 @@ const ProfileContainer = props => {
                 id='email'
                 onChange={e => setEmail(e.target.value)}
               />
-               </li>
+            </li>
             <li>
               <label htmlFor='password'>Password</label>
               <input
@@ -88,12 +89,63 @@ const ProfileContainer = props => {
                 name='password'
                 onChange={e => setPassword(e.target.value)}
               />
-          
-
-  )
-
-
-
+            </li>
+            <li>
+              <label htmlFor='password_confirmation'>Password Confirmation</label>
+              <input
+                required
+                type='password'
+                id='password_confirmation'
+                name='password_confirmation'
+                onChange={e => setPasswordConformation(e.target.value)}
+              />
+            </li>
+            <li>
+              <button type='submit' className='button primary'>
+                Update
+              </button>
+            </li>
+            <li>
+              <button type='button' onClick={handleLogout} className='button secondary full-width'>
+                Logout
+              </button>
+            </li>
+          </ul>
+        </form>
+      </div>
+      <div className='profile-orders content-margined'>
+        <h2>Your Orders</h2>
+        {!success ? (
+          <div>Loading...</div>
+        ) : (
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Date</th>
+                <th>Total</th>
+                <th>Paid</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {myNewOrders.map(order => (
+                <tr key={order.id}>
+                  <td>{order.id}</td>
+                  <td>{order.created_at.substring(0, 10)}</td>
+                  <td>{formatCurrency(order.totalPrice)}</td>
+                  <td>{order.isPaid.toString()}</td>
+                  <td>
+                    <Link to={`/orders/${order.id}`}>Details</Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default ProfileContainer;
